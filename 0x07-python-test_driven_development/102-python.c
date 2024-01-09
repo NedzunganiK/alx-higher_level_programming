d383c504a8234b211f88eab7c655160ef1d1f6b4
NedzunganiK <Nedzunganik8@gmail.com>
2024-01-08 22:10:59 -0800

    0x07. Python - Test-driven development

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,33 +1,29 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+#include "Python.h"
 
-void print_python_string(PyObject *p) {
-    if (!PyUnicode_Check(p)) {
-        PyErr_Print();
-        fprintf(stderr, "[ERROR] Invalid String Object\n");
-        return;
-    }
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
+ */
+void print_python_string(PyObject *p)
+{
+	long int length;
 
-    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-    const char *value = PyUnicode_AsUTF8(p);
+	fflush(stdout);
 
-    printf("[.] string object info\n");
-    printf("  type: %s\n", PyUnicode_IS_COMPACT_ASCII(p) ? "compact ascii" : "compact unicode object");
-    printf("  length: %zd\n", length);
-    printf("  value: %s\n", value);
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
 
-    if (!PyUnicode_IS_COMPACT_ASCII(p)) {
-        printf("  [Unicode Info]\n");
-        printf("    is_ascii: %d\n", PyUnicode_IS_ASCII(p));
-        printf("    is_compact: %d\n", PyUnicode_IS_COMPACT(p));
-        printf("    is_utf_8: %d\n", PyUnicode_IS_UTF8(p));
-    }
-}
+	length = ((PyASCIIObject *)(p))->length;
 
-int main(void) {
-    Py_Initialize();
-
-    Py_Finalize();
-    return 0;
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
 
